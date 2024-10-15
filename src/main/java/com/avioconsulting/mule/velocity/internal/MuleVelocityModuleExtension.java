c54e7883a2d62b1a9539af88082289203300bf19
Manik Magar <mmagar@avioconsulting.com>
2024-10-15 15:08:33 +0530

    feat: java 17 support

--- src/main/java/com/avioconsulting/mule/velocity/internal/MuleVelocityModuleExtension.java
+++ src/main/java/com/avioconsulting/mule/velocity/internal/MuleVelocityModuleExtension.java
@@ -3,6 +3,8 @@ package com.avioconsulting.mule.velocity.internal;
 import org.mule.runtime.extension.api.annotation.Configurations;
 import org.mule.runtime.extension.api.annotation.Extension;
 import org.mule.runtime.extension.api.annotation.dsl.xml.Xml;
+import org.mule.sdk.api.annotation.JavaVersionSupport;
+import org.mule.sdk.api.meta.JavaVersion;
 
 /**
  * This is the main class of an extension, is the entry point from which
@@ -12,6 +14,7 @@ import org.mule.runtime.extension.api.annotation.dsl.xml.Xml;
 @Xml(prefix = "velocity-template")
 @Extension(name = "Velocity Template")
 @Configurations(MuleVelocityModuleConfiguration.class)
+@JavaVersionSupport({ JavaVersion.JAVA_8, JavaVersion.JAVA_11, JavaVersion.JAVA_17 })
 public class MuleVelocityModuleExtension {
 
 }
